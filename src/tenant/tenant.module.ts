cee62a7046e72a7cad525fbe0930c38929921a60
mo <mo3789530@gmail.com>
2021-09-28 23:32:34 +0900

    remove error

--- src/tenant/tenant.module.ts
+++ src/tenant/tenant.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TenantServcieService } from './tenant-servcie/tenant-servcie.service';
-import { TenantService } from './tenant/tenant.service';
-import { TenantController } from './tenant/tenant.controller';
+import { TenantService } from './services/tenant.service';
+import { TenantController } from './controllers/tenant.controller';
 
 @Module({
-  providers: [TenantServcieService, TenantService],
-  controllers: [TenantController]
+  providers: [TenantService],
+  controllers: [TenantController],
 })
 export class TenantModule {}
